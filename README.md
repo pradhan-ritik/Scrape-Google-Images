9c1d26f9fc0a22cc0125a087ffb5e8fc254cd702
flipit001 <flipit001@gmail.com>
2024-06-11 10:04:47 -0700

    Updated code so that it uses json file as input, and it can be used as an api. updated README.md to show how it works with json files.

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Scrape-Google-Images
-Scrape images from Google Images based on a term. This can be used to generate datasets of images for research or AI.
+Scrape images from Google Images based on a term. This can be used to generate datasets of images for research or AI. It will get exactly 20 images per term.
 
 # Download
 ```bash
-git clone git@github.com:flipit001/ScrapeGoogleImages.git
+> git clone git@github.com:flipit001/ScrapeGoogleImages.git
 ```
 # Use
 ```bash
-python3 scrape_images.py {TERM} {DOWNLOAD_PATH}
+> python3 scrape_images.py {JSON FILE} {DOWNLOAD_PATH}
 ```
 ## Example
 ```bash
-python3 scrape_images.py dog ./imgs/
+> mkdir images # make sure this directory is empty
+> cat example.json
+[
+    "dog",
+    "elephant"
+]
+> python3 scrape_images.py example.json ./images/
 ```
--- scrape_images.py
+++ scrape_images.py
@@ -1,28 +1,42 @@
-import urllib.parse
+import os.path
 import requests
 import re
 import sys
-import os.path
-
-if len(sys.argv) < 3:
-    raise Exception("To use this file, run: python3 scrape_images.py {TERM} {DOWNLOAD_PATH}")
-
-term = sys.argv[1]
-path = sys.argv[2]
+import urllib.parse
+import json
 
 IMG_SEARCH = re.compile(r"\<img\sclass\=\"[^\"]*\"\salt\=\"[^\"]*\"\ssrc\=\"([^\"]*)")
 
+def scrape_google_images(terms, path):
+    for term in terms:
 
-encoded_term = urllib.parse.quote_plus(term)
+        encoded_term = urllib.parse.quote_plus(term)
 
-url = f"https://www.google.com/search?q={encoded_term}&udm=2"
+        folder_path = os.path.join(path, encoded_term)
+        os.mkdir(folder_path)
 
-page_content = requests.get(url).content.decode()
+        url = f"https://www.google.com/search?q={encoded_term}&udm=2"
 
-imgs = IMG_SEARCH.findall(page_content)
+        page_content = requests.get(url).content.decode()
 
-for idx, img in enumerate(imgs):
-    img_to_write = requests.get(img)
-    with open(os.path.join(path, f"{encoded_term}_{idx+1}.jpg"), "wb") as fh:
-        fh.write(img_to_write.content)
-    print(f"successfully downloaded {encoded_term}_{idx+1}.jpg")
+        imgs = IMG_SEARCH.findall(page_content)
+
+        for idx, img in enumerate(imgs):
+            img_to_write = requests.get(img)
+            download_path = os.path.join(folder_path, f"{encoded_term}_{idx+1}.jpg") 
+            with open(download_path, "wb") as fh:
+                fh.write(img_to_write.content)
+            print(f"successfully downloaded {download_path}")
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise Exception("To use this file, run: python3 scrape_images.py {JSON FILE} {DOWNLOAD_PATH}")
+
+    json_file = sys.argv[1]
+    path = sys.argv[2]
+
+
+    with open(json_file, "r") as fh:
+        terms = json.load(fh)
+
+    scrape_google_images(terms, path)
